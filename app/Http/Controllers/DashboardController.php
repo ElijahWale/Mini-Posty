c9dcf7acc91429aba1f63842f440460b0ed221b4
elijahwale <walexy730@gmail.com>
2021-08-30 02:19:23 +0100

    add files

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+use App\Mail\PostLiked;
+use Illuminate\support\Facades\Mail;
+
+
+class DashboardController extends Controller
+{
+    //
+    public function __construct()
+    {
+        $this->middleware(['auth']);
+    }
+
+
+    public function index(){
+       
+        return view('dashboard');
+        
+    }
+}
